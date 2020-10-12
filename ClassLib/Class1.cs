f6a24a70c9613aa06ed193720a7f5a01b4fa9169
PotatoB666 <1363964141@qq.com>
2020-10-12 15:15:06 +0000

    initial commit

---
+++ ClassLib/Class1.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace ClassLib
+{
+    [Serializable]
+    [StructLayout(LayoutKind.Sequential, Pack = 1)]
+    public struct C2PMsg
+    {
+        public sbyte type;
+        public byte[] strValue;
+        public byte[] imgValue;
+        public int len;
+    }
+
+}
